92ef29c2399f40ddd00828cb4bdb4b310a271d05
Alexander <alexmezhenskyi@gmail.com>
2017-06-06 00:40:59 +0300

    done

--- assets/scripts/media.js
+++ assets/scripts/media.js
@@ -1,5 +1,6 @@
 $(window).resize(function () {
 	if ($(this).width() >= 1200) {
+		$('.navbar-collapse').removeClass('text-center').addClass('pull-right');
 		$('.ba-about-us-left').removeClass('col-sm-offset-2 col-md-offset-2');
 		$('.ba-about-us-right').removeClass('col-sm-offset-2 col-md-offset-2');
 		$('.ba-about-us__button').removeClass('btn-block');
@@ -7,16 +8,19 @@ $(window).resize(function () {
 	} else if ($(this).width() < 1200 && $(this).width() >= 992) {
 		$('.ba-about-us-left').addClass('col-md-offset-2');
 		$('.ba-about-us-right').removeClass('pull-right').addClass('col-md-offset-2');
+		$('.navbar-collapse').removeClass('text-center').addClass('pull-right');
 		$('.ba-about-us__button').removeClass('btn-block');
 		$('.ba-works-btn').removeClass('btn-block');
 	} else if ($(this).width() < 992 && $(this).width() >= 768) {
 		$('.ba-about-us-left').addClass('col-sm-offset-2');
 		$('.ba-about-us-right').addClass('col-sm-offset-2');
+		$('.navbar-collapse').removeClass('text-center').addClass('pull-right');
 		$('.ba-about-us__button').removeClass('btn-block');
 		$('.ba-works-btn').removeClass('btn-block');
 	} else if ($(this).width() < 768) {
 		$('.ba-about-us__button').addClass('btn-block');
 		$('.ba-works-btn').addClass('btn-block');
+		$('.navbar-collapse').removeClass('pull-right').addClass('text-center');
 	}
 });
 
